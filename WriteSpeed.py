b56cca533c5c3994edeeff3df6141d1627433671
ForbiddenGeorge <j.holik@gymzn.cz>
2023-02-10 17:22:06 +0100

    Pr8ce na rychlopsaní

--- WriteSpeed.py
+++ WriteSpeed.py
@@ -5,7 +5,12 @@ import flet as ft
 def Rychlost(page, ft=ft):
     content= ft.Column(
         [
-            ft.Text("Woooow", size=100)
+            ft.Row(
+                [
+                    ft.Text("Lorem Ipsum", size=100)
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
         ]
     )
     return content
