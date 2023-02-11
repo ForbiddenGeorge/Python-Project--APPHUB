8a6eea962784348428a0f16e5d4fa9aa26ad09b4
ForbiddenGeorge <j.holik@gymzn.cz>
2023-02-11 12:30:02 +0100

    Práce na rychlopsaní

--- WriteSpeed.py
+++ WriteSpeed.py
@@ -3,14 +3,51 @@ import flet as ft
 
 
 def Rychlost(page, ft=ft):
+    def Napis(e):
+        veta.value = "It's Over, Anakin. I Have The High Ground!!"
+        pole.value = ""
+        page.update()
+
+    def Test(e):
+        veta.value = "Konec testu"
+        page.update()
+
+    veta =ft.Text(selectable=False, value="", text_align=ft.TextAlign.CENTER, size=30, weight=ft.FontWeight.BOLD)
+    pole = ft.TextField(width=700, label="Text", on_submit=Test)
     content= ft.Column(
         [
             ft.Row(
                 [
-                    ft.Text("Lorem Ipsum", size=100)
+                    ft.Text("Přepište větu co nejrychleji dokážete", size=22)
                 ],
                 alignment=ft.MainAxisAlignment.CENTER
-            )
+            ),
+            ft.Row(
+                [
+                    ft.Text("Kliknutím na pole začne časovač počítem, stisknutím klávesy Enter pokus ukončíte", size=16)
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            ),
+            ft.Row(
+                [
+                   veta
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            ),
+            ft.Row(
+                [
+                    pole
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            ),
+            ft.Row(
+                [
+                    ft.ElevatedButton(text="Restartovat", on_click=Napis)
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            ),
+
+
         ]
     )
     return content
