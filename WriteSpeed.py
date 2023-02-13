d4422c2effdf9418492ef95fd8b5a987631d1d37
ForbiddenGeorge <j.holik@gymzn.cz>
2023-02-13 20:32:29 +0100

    Práce na rychlopsaní

--- WriteSpeed.py
+++ WriteSpeed.py
@@ -1,19 +1,40 @@
+import random
+import time
+
 import flet as ft
-
-
 
 def Rychlost(page, ft=ft):
     def Napis(e):
-        veta.value = "It's Over, Anakin. I Have The High Ground!!"
+        f = open('vety.txt').read()
+        sentences = f.split('\n')
+        veta.value = random.choice(sentences)
         pole.value = ""
         page.update()
+
 
     def Test(e):
         veta.value = "Konec testu"
         page.update()
 
+    def Cas(e):
+        while pole.on_focus:
+            start = time.time()
+            end = time.time()
+            sec = start - end
+            mins = sec // 60
+            sec = sec % 60
+            hours = mins // 60
+            mins = mins % 60
+            cas.value = "{0}:{1}:{2}".format(int(hours),int(mins),sec)
+            print( cas.value)
+            time.sleep(1)
+            page.update()
+
+
     veta =ft.Text(selectable=False, value="", text_align=ft.TextAlign.CENTER, size=30, weight=ft.FontWeight.BOLD)
-    pole = ft.TextField(width=700, label="Text", on_submit=Test)
+    pole = ft.TextField(width=700, label="Text", on_submit=Test, on_focus=Cas)
+    cas = ft.Text(size=45, weight=ft.FontWeight.BOLD, value= "0:0:0.0")
+    spravnost = ft.Text(size=30, weight=ft.FontWeight.W_200, value="Správnost pokusu")
     content= ft.Column(
         [
             ft.Row(
@@ -24,7 +45,7 @@ def Rychlost(page, ft=ft):
             ),
             ft.Row(
                 [
-                    ft.Text("Kliknutím na pole začne časovač počítem, stisknutím klávesy Enter pokus ukončíte", size=16)
+                    ft.Text("Kliknutím na pole začne časovač počítat, stisknutím klávesy Enter pokus ukončíte", size=16)
                 ],
                 alignment=ft.MainAxisAlignment.CENTER
             ),
@@ -46,6 +67,18 @@ def Rychlost(page, ft=ft):
                 ],
                 alignment=ft.MainAxisAlignment.CENTER
             ),
+            ft.Row(
+                [
+                    cas
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            ),
+            ft.Row(
+                [
+                    spravnost
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
 
 
         ]
