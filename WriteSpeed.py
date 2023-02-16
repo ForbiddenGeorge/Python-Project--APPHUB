963d16ff5e951cf8c4fa59bdadc8e3fb6baf2016
ForbiddenGeorge <j.holik@gymzn.cz>
2023-02-16 12:45:36 +0100

    Okomentování kódu

--- WriteSpeed.py
+++ WriteSpeed.py
@@ -7,12 +7,16 @@ import flet as ft
 
 
 def Rychlost(page, ft=ft):
-
+    '''Hlavní flunkce volaná řídícím skriptem
+    Skript pro kontrolu rychlosti a přesnosti psaní
+    '''
 
     def Napis(e):
-        f = open('vety.txt').read()
-        sentences = f.split('\n')
+        '''Tato funkce vybere z textového souboru náhodnou testovací větu a tu následně zobrazí na obrazovce'''
+        f = open('vety.txt').read() #Oteření souboru s větami
+        sentences = f.split('\n') #Rozdělení jednotlivých vět po řádcích
         veta.value = random.choice(sentences)
+        '''Vynulování všech hodnot pro novou větu'''
         pole.value = ""
         spravnost.value = "Správnost: "
         cas.value = "0:0.0"
@@ -20,49 +24,51 @@ def Rychlost(page, ft=ft):
 
 
     def Test(e):
+        '''Funkce kotrolujíí přesnost přepisu'''
         pocet = 0
         for i, c in enumerate(str(veta.value)):
             try:
-                if pole.value[i] == c:
+                if pole.value[i] == c: #Pokud se charakter věty rovná charakteru napsané věty, přičtě se 1 k počtu správných charakterů, jinak se nic neděje
                     pocet += 1
             except:
                 pass
-        presnost = pocet / len(veta.value) * 100
+        presnost = pocet / len(veta.value) * 100 #počet správných charakterů / celkovým počtem charakterů * 100 abychom to měli v procenteech
         presnost = round(presnost, 2)
 
-        spravnost.value = "Správnost: {0}".format(presnost) + "%"
+        spravnost.value = "Správnost: {0}".format(presnost) + "%" #Vypsání procentuálního výsledku
         page.update()
 
 
     def Cas(e):
+        '''Stopkyw'''
         setina = 0
         sekunda = 0
         minuta = 0
         if pole.value != "":
             pass
         else:
-            while not keyboard.is_pressed("enter"):
+            while not keyboard.is_pressed("enter"): #Dokud není stiknut Enter, časovač stále běží
 
                 setina = setina +1
                 time.sleep(0.01)
-                if setina == 100:
+                if setina == 100: #Za každých sto setin se připíše jedna sekunda a setiny se vynulují
                     setina = 0
                     sekunda = sekunda + 1
-                if sekunda == 60:
+                if sekunda == 60: # Za 60 vteřín se přičte minuta a sekundy se vynulují
                     sekunda = 0
                     minuta = minuta + 1
-                cas.value = "{0}:{1}.{2}".format(int(minuta), int(sekunda), int(setina))
+                cas.value = "{0}:{1}.{2}".format(int(minuta), int(sekunda), int(setina)) #Vypsání nového času na obrazovku
                 page.update()
             else:
                 cas.value = "{0}:{1}.{2}".format(int(minuta), int(sekunda), int(setina))
                 page.update()
 
 
-    veta =ft.Text(selectable=False, value="", text_align=ft.TextAlign.CENTER, size=30, weight=ft.FontWeight.BOLD)
-    pole = ft.TextField(width=700, label="Text", on_submit=Test, on_focus=Cas)
-    cas = ft.Text(size=45, weight=ft.FontWeight.BOLD, value= "0:0.0")
-    spravnost = ft.Text(size=30, weight=ft.FontWeight.W_200, value="Správnost pokusu")
-    content= ft.Column(
+    veta =ft.Text(selectable=False, value="", text_align=ft.TextAlign.CENTER, size=30, weight=ft.FontWeight.BOLD) #Hlavní testovací věta
+    pole = ft.TextField(width=700, label="Text", on_submit=Test, on_focus=Cas) #Pole kam uživatel přepisuje větu
+    cas = ft.Text(size=45, weight=ft.FontWeight.BOLD, value= "0:0.0") # zobrazení času
+    spravnost = ft.Text(size=30, weight=ft.FontWeight.W_200, value="Správnost pokusu") #Text pro výsledné zobrazení textu
+    content= ft.Column( # Grafické rozložení aplikace
         [
             ft.Row(
                 [
